4157220884bb6760bb995e34a91d31d4fa19084d
MihaelaDanila100 <alexandrad.mihaela@gmail.com>
2024-11-23 22:17:08 +0200

    added dialog component

---
+++ web-app/src/components/dialog.ts
@@ -0,0 +1,8 @@
+import { LionDialog } from '@lion/ui/dialog.js';
+import { CSSResult } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('app-dialog')
+export class AppDialog extends LionDialog {
+
+}
--- web-app/src/components/header.ts
+++ web-app/src/components/header.ts
@@ -1,23 +1,40 @@
 import { CSSResult, html, HTMLTemplateResult, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, state } from "lit/decorators.js";
 import { HeaderStyles } from "../styles/header-styles.js";
 import './button.js';
+import './dialog.js';
 import { THEME_COLORS } from "../styles/theme.js";
 
 @customElement('app-header')
 export class HeaderComponent extends LitElement {
 
+    @state()
+    private _isOpen: boolean = false;
+
     static override get styles(): [CSSResult] {
         return [HeaderStyles];
     }
 
+    private _toggleDialog = (newIsOpen: boolean) => {
+        this._isOpen = newIsOpen;
+    }
+
     protected render(): HTMLTemplateResult {
+        
         return html`
             <header class = "header-container">
-                <app-button .buttonColor = ${THEME_COLORS.primaryLightWhite.cssText}
+                <app-button
+                        @click = ${this._toggleDialog(true)}
+                        .buttonColor = ${THEME_COLORS.primaryLightWhite.cssText}
                         .textColor = ${THEME_COLORS.primaryBlue.cssText}>
                     Log In
                 </app-button>
+
+                <lion-dialog ?opened=${this._isOpen}>
+                    <div slot="content" class="dialog">
+                        Hello dialog!
+                    </div>
+                </lion-dialog>
             </header>
         `;
     }
