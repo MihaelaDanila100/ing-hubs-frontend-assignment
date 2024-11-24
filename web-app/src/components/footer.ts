de915438a3cb3175939d9d646cebb19494fe6827
MihaelaDanila100 <alexandrad.mihaela@gmail.com>
2024-11-24 18:12:12 +0200

    added contact in footer

--- web-app/src/components/footer.ts
+++ web-app/src/components/footer.ts
@@ -1,12 +1,31 @@
 import { CSSResult, html, LitElement, TemplateResult } from "lit";
 import { customElement } from "lit/decorators.js";
 import { FooterStyles } from "../styles/footer-styles.js";
+import { FooterData } from "../interfaces/footer-data.js";
 
 @customElement('app-footer')
 export class AppFooter extends LitElement {
 
     static override get styles(): [CSSResult] {
         return [FooterStyles];
+    }
+
+    private _iconsList: FooterData[] = [
+        {
+            text: 'Bucharest, Sector 4, Bd. Bulevard'
+        }, {
+            text: 'info@contact.com'
+        }, {
+            text: '+1234567'
+        }
+    ];
+
+    private _renderIconDetail = (iconItem: FooterData): TemplateResult<1> => {
+        return html`
+            <div>
+                ${iconItem.text}
+            </div>
+        `;
     }
     
     protected render(): TemplateResult<1> {
@@ -19,6 +38,9 @@ export class AppFooter extends LitElement {
                 </div>
 
                 <div class='footer-data'>
+                    <h3>Contact</h3>
+                    <hr>
+                    ${this._iconsList.map((iconItem: FooterData) => (html`${this._renderIconDetail(iconItem)}`))}
                 </div>
             </footer>
         `;
