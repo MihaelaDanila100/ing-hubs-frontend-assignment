1cb8acd656940a6864727dbee7eed913ff9e1433
MihaelaDanila100 <alexandrad.mihaela@gmail.com>
2024-11-24 19:56:26 +0200

    added user component

---
+++ web-app/src/styles/user-details-style.ts
@@ -0,0 +1,12 @@
+import { css, CSSResult } from "lit";
+
+export const UserDetailsStyle: CSSResult = css`
+    .detail-row {
+        font-size: 1.2rem;
+        margin: 1rem;
+    }
+
+    .detail-label {
+        font-weight: 600;
+    }
+`;
--- web-app/src/web-app.ts
+++ web-app/src/web-app.ts
@@ -3,6 +3,7 @@ import { customElement, state } from 'lit/decorators.js';
 import './components/header.js';
 import './components/login-form.js';
 import './components/footer.js';
+import './components/user-details.js';
 import { AuthService } from './services/auth-service.js';
 
 @customElement('web-app')
@@ -26,7 +27,8 @@ export class WebApp extends LitElement {
     return html`
       <app-header .isLogIn = ${this._isLogIn}
         @onLogOut=${() => this._updateTemplate(false)}></app-header>
-      <app-login-form @onLogIn=${() => this._updateTemplate(true)}></app-login-form>
+      ${this._isLogIn ? html`<app-user-details></app-user-details>` : html`<app-login-form @onLogIn=${() => this._updateTemplate(true)}></app-login-form>`}
+      
       <app-footer></app-footer>
     `;
   }
